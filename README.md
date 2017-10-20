09cbc4e9ad475f11b0bda2699c7955a4d45211ee
Nino Kodabande <nkodabande@suse.com>
2017-10-20 15:47:56 -0700

    Allow to Specify Cipher suite
    [#151887364]

--- README.md
+++ README.md
@@ -22,3 +22,11 @@ Before running `cf push`
     ```
 - Include `server.crt` and `server.key` files in this directory
 - Include a `ca.crt` file in this directory (CA that signed the expected client certs)
+
+## Specifying Cipher suites while TLS or MTLS is enabled
+- When no cipher suite is provided, the [golang default](https://golang.org/pkg/crypto/tls/#pkg-constants) ciphers are loaded.
+- When the set of ciphers are specified and shared with the gorouter, the most preferred cipher (first one in the list) is used.
+- In `manifest.yml`
+    ```yaml
+    CIPHER: "ECDHE-RSA-AES128-GCM-SHA256:TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+    ```
--- site.go
+++ site.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
@@ -21,6 +22,7 @@ func main() {
 	port := os.Getenv("PORT")
 	tlsEnv := os.Getenv("TLS")
 	mtlsEnv := os.Getenv("MTLS")
+	cipherSuite := os.Getenv("CIPHER")
 
 	tlsEnabled := tlsEnv != "false"
 	mtlsEnabled := mtlsEnv != "false"
@@ -28,6 +30,13 @@ func main() {
 		log.Fatal("invalid config: mtls requires tls")
 	}
 	tlsConfig := &tls.Config{}
+	if tlsEnabled {
+		if cipherSuite != "" {
+			ciphers := strings.Split(cipherSuite, ":")
+			tlsConfig.CipherSuites = cipherValue(ciphers)
+		}
+
+	}
 	if tlsEnabled && mtlsEnabled {
 		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
 		certPool, err := x509.SystemCertPool()
@@ -56,4 +65,61 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func cipherValue(ciphers []string) []uint16 {
+	cipherMap := map[string]uint16{
+		"RC4-SHA":                                 0x0005, // openssl formatted values
+		"DES-CBC3-SHA":                            0x000a,
+		"AES128-SHA":                              0x002f,
+		"AES256-SHA":                              0x0035,
+		"AES128-SHA256":                           0x003c,
+		"AES128-GCM-SHA256":                       0x009c,
+		"AES256-GCM-SHA384":                       0x009d,
+		"ECDHE-ECDSA-RC4-SHA":                     0xc007,
+		"ECDHE-ECDSA-AES128-SHA":                  0xc009,
+		"ECDHE-ECDSA-AES256-SHA":                  0xc00a,
+		"ECDHE-RSA-RC4-SHA":                       0xc011,
+		"ECDHE-RSA-DES-CBC3-SHA":                  0xc012,
+		"ECDHE-RSA-AES128-SHA":                    0xc013,
+		"ECDHE-RSA-AES256-SHA":                    0xc014,
+		"ECDHE-ECDSA-AES128-SHA256":               0xc023,
+		"ECDHE-RSA-AES128-SHA256":                 0xc027,
+		"ECDHE-RSA-AES128-GCM-SHA256":             0xc02f,
+		"ECDHE-ECDSA-AES128-GCM-SHA256":           0xc02b,
+		"ECDHE-RSA-AES256-GCM-SHA384":             0xc030,
+		"ECDHE-ECDSA-AES256-GCM-SHA384":           0xc02c,
+		"ECDHE-RSA-CHACHA20-POLY1305":             0xcca8,
+		"ECDHE-ECDSA-CHACHA20-POLY1305":           0xcca9,
+		"TLS_RSA_WITH_RC4_128_SHA":                0x0005, // RFC formatted values
+		"TLS_RSA_WITH_3DES_EDE_CBC_SHA":           0x000a,
+		"TLS_RSA_WITH_AES_128_CBC_SHA":            0x002f,
+		"TLS_RSA_WITH_AES_256_CBC_SHA":            0x0035,
+		"TLS_RSA_WITH_AES_128_CBC_SHA256":         0x003c,
+		"TLS_RSA_WITH_AES_128_GCM_SHA256":         0x009c,
+		"TLS_RSA_WITH_AES_256_GCM_SHA384":         0x009d,
+		"TLS_ECDHE_ECDSA_WITH_RC4_128_SHA":        0xc007,
+		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA":    0xc009,
+		"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA":    0xc00a,
+		"TLS_ECDHE_RSA_WITH_RC4_128_SHA":          0xc011,
+		"TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA":     0xc012,
+		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":      0xc013,
+		"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":      0xc014,
+		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256": 0xc023,
+		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256":   0xc027,
+		"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256":   0xc02f,
+		"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256": 0xc02b,
+		"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384":   0xc030,
+		"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384": 0xc02c,
+		"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305":    0xcca8,
+		"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305":  0xcca9,
+	}
+
+	var cipherVals []uint16
+	for _, c := range ciphers {
+		if val, ok := cipherMap[c]; ok {
+			cipherVals = append(cipherVals, val)
+		}
+	}
+	return cipherVals
 }
